7330765741fcd28feeeb96706f13693a9461e598
Nate Johnson <138185067+Johnsonbros@users.noreply.github.com>
2026-01-14 13:50:01 -0500

    Add testing setup and chat card coverage

---
+++ test/setup.ts
@@ -0,0 +1,32 @@
+import "@testing-library/jest-dom/vitest";
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
+
+Object.defineProperty(window, "ResizeObserver", {
+  writable: true,
+  value: ResizeObserverMock,
+});
+
+Object.defineProperty(navigator, "clipboard", {
+  value: {
+    writeText: async () => {},
+  },
+});
